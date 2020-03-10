7e95167818605617496eed965979a8715ce46474
Adam <adameli22@gmail.com>
2020-03-10 19:41:54 +0000

    third commit

--- sample.py
+++ sample.py
@@ -1,14 +1,17 @@
 import pytest
 
 def FizzBuzz(x):
-    if x % 3 and x % 5:
+    if x % 3 == 0 and x % 5 == 0:
         return "FizzBuzz"
+    elif x % 5 == 0:
+            return "Buzz"
 
-
+    
 
 
 
 
 def test_answers():
-    assert FizzBuzz(6) == "FizzBuzz"
-
+    assert FizzBuzz(15) == "FizzBuzz"
+    assert FizzBuzz(10) == "Buzz"
+    
